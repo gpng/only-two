37e43f33e66794694da42cc124ea9172b0f67687
Gerald Png <gpng@estl.sg>
2021-07-19 16:17:20 +0800

    Add ga

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {process.env.NODE_ENV === 'production' && (
+            <>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-B8FPVMN7GS" />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'G-B8FPVMN7GS');
+              `,
+                }}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
